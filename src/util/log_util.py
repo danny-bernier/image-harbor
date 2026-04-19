4c811cdf8b1873cc032ca354d1d6b89fdf07dfe1
danny-bernier <79473002+danny-bernier@users.noreply.github.com>
2026-04-18 21:09:29 -0400

    made timestamp format configurable

--- src/util/log_util.py
+++ src/util/log_util.py
@@ -5,6 +5,7 @@ from logging import Logger
 _DEFAULT_LOG_LEVEL = "INFO"
 _LOG_FORMAT = "%(asctime)s %(levelname)s [%(name)s] %(message)s"
 _ENVIRONMENT_LOG_LEVEL = "IMAGE_HARBOR_LOG_LEVEL"
+_ENVIRONMENT_LOG_TIMESTAMP_FORMAT = "IMAGE_HARBOR_LOG_TIMESTAMP_FORMAT"
 
 
 def get_logger(name: str) -> Logger:
@@ -13,9 +14,10 @@ def get_logger(name: str) -> Logger:
     if not root_logger.handlers:
         log_level_name = os.getenv(_ENVIRONMENT_LOG_LEVEL, _DEFAULT_LOG_LEVEL).upper()
         log_level = getattr(logging, log_level_name, logging.INFO)
+        timestamp_format = os.getenv(_ENVIRONMENT_LOG_TIMESTAMP_FORMAT)
 
         handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter(_LOG_FORMAT))
+        handler.setFormatter(logging.Formatter(_LOG_FORMAT, datefmt=timestamp_format))
 
         root_logger.addHandler(handler)
         root_logger.setLevel(log_level)
